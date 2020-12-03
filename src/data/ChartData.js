c77e0be81e405e4566d7d78cb563a6dc46b1cba9
mechines <songyu19@outlook.com>
2020-12-03 13:52:07 +0800

    更大的缩放比例

--- src/data/ChartData.js
+++ src/data/ChartData.js
@@ -37,7 +37,7 @@ export const DrawActionType = {
 }
 
 const MAX_DATA_SPACE = 50
-const MIN_DATA_SPACE = 3
+const MIN_DATA_SPACE = 0.00001
 
 export default class ChartData {
   constructor (styleOptions, invalidateHandler) {
